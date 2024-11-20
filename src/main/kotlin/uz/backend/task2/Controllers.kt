ca56dc5601557211434350e9faf16ea818b4aa38
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-11-20 17:07:08 +0500

    USER, CATEGORY, PRODUCT controller add

--- src/main/kotlin/uz/backend/task2/Controllers.kt
+++ src/main/kotlin/uz/backend/task2/Controllers.kt
@@ -1,5 +1,6 @@
 package uz.backend.task2
 
+import jakarta.validation.Valid
 import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
@@ -16,7 +17,57 @@ class ExceptionHandler(private val errorMessageSource: ResourceBundleMessageSour
 
     @RestController
     @RequestMapping("/api/v1/user")
-    class UserController() {
+    class UserController(val service: UserService) {
+        @PostMapping("create")
+        fun create(@RequestBody @Valid request: UserCreateRequest) = service.create(request)
 
+        @GetMapping
+        fun getAll(pageable: Pageable) = service.getAll(pageable)
+
+
+        @GetMapping("{id}")
+        fun getOne(@PathVariable id: Long) = service.getOne(id)
+
+        @PostMapping("{id}")
+        fun update(@PathVariable id: Long, @RequestBody request: UserUpdateRequest) = service.update(id, request)
+
+        @DeleteMapping("{id}")
+        fun delete(@PathVariable id: Long) = service.delete(id)
+    }
+
+    @RestController
+    @RequestMapping("/api/v1/category")
+    class CategoryController(val service: CategoryService) {
+        @PostMapping("create")
+        fun create(@RequestBody @Valid request: CategoryCreateRequest) = service.create(request)
+
+        @GetMapping
+        fun getAll(pageable: Pageable) = service.getAll(pageable)
+
+        @GetMapping("{id}")
+        fun getOne(@PathVariable id: Long) = service.getOne(id)
+
+        @PostMapping("{id}")
+        fun update(@PathVariable id: Long, @RequestBody request: CategoryUpdateRequest) = service.update(id, request)
+
+    }
+
+    @RestController
+    @RequestMapping("/api/v1/product")
+    class ProductController(val service: ProductService) {
+        @PostMapping("create")
+        fun create(@RequestBody @Valid request: ProductCreateRequest) = service.create(request)
+
+        @GetMapping
+        fun getAll(pageable: Pageable) = service.getAll(pageable)
+
+        @GetMapping("{id}")
+        fun getOne(@PathVariable id: Long) = service.getOne(id)
+
+        @PostMapping("{id}")
+        fun update(@PathVariable id: Long, @RequestBody request: ProductUpdateRequest) = service.update(id, request)
+
+        @DeleteMapping("{id}")
+        fun delete(@PathVariable id: Long) = service.delete(id)
     }
 }
